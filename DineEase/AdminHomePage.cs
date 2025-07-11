c55d4c093ea4b6403ae7124e75e137b5f398ba00
Imasha <imashaudayanganie2020@gmail.com>
2025-07-11 21:31:36 +0530

    I created UI and implement create operation

---
+++ DineEase/AdminHomePage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DineEase
+{
+    public partial class AdminHomePage : Form
+    {
+        string connectionString = @"Data Source=DESKTOP-TAR59NP\SQLEXPRESS;Initial Catalog=dineEase;Integrated Security=True";
+        public AdminHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ButtonAddNewItem_Click(object sender, EventArgs e)
+        {
+            AddItemPage addItemPage = new AddItemPage(); // create instance of form2
+            addItemPage.Show(); // open the AddItemPage
+            this.Hide(); // optional: hide AdminHomePage
+        }
+    }
+}
