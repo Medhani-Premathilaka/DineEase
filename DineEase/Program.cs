a120cdb2f118efd6c305d430511b56076e3cd1a5
medhani premathilaka <mpremathilaka1@gmail.com>
2025-07-12 19:33:02 +0530

    #updated

--- DineEase/Program.cs
+++ DineEase/Program.cs
@@ -13,7 +13,7 @@ namespace DineEase
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminHomepage());
+            Application.Run(new Form1());
         }
     }
 }
