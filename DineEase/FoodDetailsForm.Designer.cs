062015cdebeb8339334883c7425de83eb9cb38a0
Ishini <misnfernando1@gmail.com>
2025-07-14 19:40:14 +0530

    full functionality created for userviewproduct form

---
+++ DineEase/FoodDetailsForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DineEase
+{
+    partial class FoodDetailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.btnIncrease = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDecrease = new Guna.UI2.WinForms.Guna2Button();
+            this.btnAddToOrder = new Guna.UI2.WinForms.Guna2Button();
+            this.txtCustomer = new Guna.UI2.WinForms.Guna2TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.FillColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ImageRotate = 0F;
+            this.pictureBox1.Location = new System.Drawing.Point(269, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseTransparentBackground = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(374, 234);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 20);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "name";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(380, 398);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(30, 20);
+            this.lblQuantity.TabIndex = 4;
+            this.lblQuantity.Text = "qty";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(378, 323);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(46, 20);
+            this.lblDesc.TabIndex = 5;
+            this.lblDesc.Text = "Desc";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(380, 278);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(43, 20);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "price";
+            // 
+            // btnIncrease
+            // 
+            this.btnIncrease.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnIncrease.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnIncrease.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnIncrease.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnIncrease.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnIncrease.ForeColor = System.Drawing.Color.White;
+            this.btnIncrease.Location = new System.Drawing.Point(495, 385);
+            this.btnIncrease.Name = "btnIncrease";
+            this.btnIncrease.Size = new System.Drawing.Size(180, 45);
+            this.btnIncrease.TabIndex = 7;
+            this.btnIncrease.Text = "+";
+            this.btnIncrease.Click += new System.EventHandler(this.btnIncrease_Click);
+            // 
+            // btnDecrease
+            // 
+            this.btnDecrease.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDecrease.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDecrease.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDecrease.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDecrease.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnDecrease.ForeColor = System.Drawing.Color.White;
+            this.btnDecrease.Location = new System.Drawing.Point(115, 398);
+            this.btnDecrease.Name = "btnDecrease";
+            this.btnDecrease.Size = new System.Drawing.Size(180, 45);
+            this.btnDecrease.TabIndex = 8;
+            this.btnDecrease.Text = "-";
+            this.btnDecrease.Click += new System.EventHandler(this.btnDecrease_Click);
+            // 
+            // btnAddToOrder
+            // 
+            this.btnAddToOrder.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddToOrder.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddToOrder.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddToOrder.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddToOrder.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnAddToOrder.ForeColor = System.Drawing.Color.White;
+            this.btnAddToOrder.Location = new System.Drawing.Point(454, 505);
+            this.btnAddToOrder.Name = "btnAddToOrder";
+            this.btnAddToOrder.Size = new System.Drawing.Size(180, 45);
+            this.btnAddToOrder.TabIndex = 9;
+            this.btnAddToOrder.Text = "Order";
+            this.btnAddToOrder.Click += new System.EventHandler(this.btnAddToOrder_Click);
+            // 
+            // txtCustomer
+            // 
+            this.txtCustomer.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtCustomer.DefaultText = "";
+            this.txtCustomer.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtCustomer.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtCustomer.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtCustomer.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtCustomer.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtCustomer.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtCustomer.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtCustomer.Location = new System.Drawing.Point(91, 490);
+            this.txtCustomer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCustomer.Name = "txtCustomer";
+            this.txtCustomer.PlaceholderText = "";
+            this.txtCustomer.SelectedText = "";
+            this.txtCustomer.Size = new System.Drawing.Size(286, 60);
+            this.txtCustomer.TabIndex = 10;
+            // 
+            // FoodDetailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.txtCustomer);
+            this.Controls.Add(this.btnAddToOrder);
+            this.Controls.Add(this.btnDecrease);
+            this.Controls.Add(this.btnIncrease);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FoodDetailsForm";
+            this.Text = "FoodDetailsForm";
+            this.Load += new System.EventHandler(this.FoodDetailsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblPrice;
+        private Guna.UI2.WinForms.Guna2Button btnIncrease;
+        private Guna.UI2.WinForms.Guna2Button btnDecrease;
+        private Guna.UI2.WinForms.Guna2Button btnAddToOrder;
+        private Guna.UI2.WinForms.Guna2TextBox txtCustomer;
+    }
+}
